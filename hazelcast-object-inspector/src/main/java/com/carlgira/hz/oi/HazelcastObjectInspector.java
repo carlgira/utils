a7a7cf15c71fc199e867228fd6cd2e1f4ca834e7
carlgira <carlgira@192.168.1.33>
2015-05-15 20:04:14 +0200

    - Hazelcast Object Inspector

--- hazelcast-object-inspector/src/main/java/com/carlgira/hz/oi/HazelcastObjectInspector.java
+++ hazelcast-object-inspector/src/main/java/com/carlgira/hz/oi/HazelcastObjectInspector.java
@@ -1,12 +1,9 @@
 package com.carlgira.hz.oi;
 
-import com.hazelcast.core.DistributedObject;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import inspectorjay.InspectorJay;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 
 /**
  * Class to view all the objects in the datagrid of Hazelcast
@@ -42,18 +39,6 @@ public class HazelcastObjectInspector
             }
         });
        inspectorjay.InspectorJay.inspect(distributedObjects);
-        
-        //Object panel = inspectorjay.InspectorJay.inspectorPanel(distributedObjects);
-        
-        //InspectorJay inspectorJay = new InspectorJay();
-        //inspectorJay.
-        //Object panel  = inspectorJay.inspectorPanel(distributedObjects);
-                
-                //..ijavaInspectorPanel(distributedObjects);
-        
-       // LinkedList l = (LinkedList)panel;
-        
-        //System.out.println("PANEL " + panel.getClass() + l.size());
         
         thread.start();
     }
